31d55a28ea476b7d5bceeb4b08761339fda28a99
A-Anirudh <70565463+A-Anirudh@users.noreply.github.com>
2022-10-16 17:08:31 +0530

    Delete predict directory

--- predict/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import *
-
-class ImageForm(forms.ModelForm):
-    class Meta:
-        model = ImageModel
-        fields = ['image_entry']
--- predict/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class ImageModel(models.Model):
-    image_entry = models.ImageField(upload_to='cricketImages')
-
-class ResultImageModel(models.Model):
-    result_image = models.ImageField(upload_to='results')
